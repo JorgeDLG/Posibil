ff8206922989fdbb24b1d8a8718ee8b42601af2e
dlag <idroj07@gmail.com>
2019-07-07 21:29:08 +0200

    Initial Commit

---
+++ posibil.js
@@ -0,0 +1,19 @@
+"strict mode";
+
+
+
+
+
+   /* ..INSTRUCCIONES..
+1. Leer palabra del input
+2. Iniciar loop con char a* en 1º pos
+3. Guardar estado del string
+4. Bloquear hasta penultimo char y conmutar chars no bloqueados. 
+   (al ser 3 el length del string solo hace 1 conmutacion.
+5. Iterar el loop a* en 2º pos
+6. Guardar estado del string
+7.  PASO 4 BIS
+8. Iterar el loop a* en 3º pos
+9. PASO 4 BIS
+10. el interador a revasado a la ultima posicion del string luego fin loop fin programa.
+*/
