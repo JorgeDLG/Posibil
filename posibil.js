9a81ea68310809a71c22acad8698bd229a3bff6e
dlag <idroj07@gmail.com>
2019-07-11 15:39:36 +0200

    snaps como array y func Commut

--- posibil.js
+++ posibil.js
@@ -7,27 +7,32 @@ function button(){
    
    //2 opciones para esta func: O se "computan" todas las snaps y printea todo d golpe
                               // O Guardar snap =  Printear (sin sobreescribe)
+   
+   //const buton = document.getElementById("boton");
+   const input = document.getElementById("input").value;
+   const out= document.getElementById("espacio"); //.value?
+   
+   var Orig = input;
+   var snaps = [,];
+
    function printear(string){ //sobreescribe
       out.innerHTML = string;
    }
 
-   //const buton = document.getElementById("boton");
-   const input = document.getElementById("input").value;
-   const out= document.getElementById("espacio");
-   const tamñ = input.length;
+   function Conmuta(){
 
-   
+   }
+   for (var i = 0; i < input.length;i++){
+      
+      var charBlq = input[i];
+      snaps[i,0] = Orig; //Original siempre va a ser 0
 
-   for (var i = 0; i < tamñ;i++){
-
-      var charBlq = input[0];
-
-      var Orig = //iria esto dentro de loop? (3-4-5)
+      Conmuta();
 
       
 
-      printear(input);
-      printear(tamñ);
+      //REVALORIZAR Orig POR CADA ITERACION al final!
+
    }
    
 }
