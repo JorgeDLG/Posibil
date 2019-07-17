c6b27585b7f890be11be8c8c3fec28849bf78884
dlag <idroj07@gmail.com>
2019-07-17 22:19:51 +0200

    S(2) parece que funcionando

--- posibil.js
+++ posibil.js
@@ -1,6 +1,6 @@
 "strict mode";
-debugger;
 alert("script launched");
+
 function test(){
 // crear arrays 2 dimensiones https://tutobasico.com/multidimensionales-javascript/
    
@@ -40,17 +40,26 @@ function Factorial(numero){
       return numero * Factorial(numero-1);
    }
 }
-function formatearAhorizontal(array,bloqs,Jcolu){
-   var B,j; //iteradores Bloqueos y J.
-   var newBlq = [],newJ = [],newArr = [];
+function desplazar(charBlq,string){
+   
+   let stringArray = string.split('');
+
+   let chAdesp = stringArray.indexOf(charBlq); //PROBLEMA con chars iguales
+
+   let aux = stringArray[chAdesp];
+
+   stringArray[chAdesp] = stringArray[chAdesp+1];
+   stringArray[chAdesp+1] = aux;
+
+   let stDesplazada = stringArray.join("");
+   return stDesplazada;
+}
+function formatearAhorizontal(matriz){ //repasa el paso de parametros(en este caso matriz)
+   var matriz = null;
 
    
-   for (B = 0; B < bloqs.length; B++) {
-      for (let j = 0; j < Jcolu.length; j++) {
-         
-         
-      }
-   }
+
+   return matriz;
 }
 function button(){
    debugger;
@@ -60,62 +69,55 @@ function button(){
    var workChar; //el que se va añadiendo
    var palHijo = palabra[0];
    var longHijo = palHijo.length;
-   var bloqueos = [] , jCol = [];
-   var snapsBlqs = [bloqueos][jCol];
+   //    CREAMOS LA MATRIZ EN EJECUCUION PARA DARLE EL TAMAÑO VARIABLE ADECUADO
+   // var  jCol = [];
+   // var snapsBlqs = [bloqueos][jCol];
    var nBlqsEnI;
-   var nBloq; //0 porque es una variable para indexar un ARRAY
    const out= document.getElementById("espacio");
-
-   function desplazar(charBlq,string){
-      
-      let stringArray = string.split('');
-
-      let chAdesp = stringArray.indexOf(charBlq); //PROBLEMA con chars iguales
-
-      let aux = stringArray[chAdesp];
-
-      stringArray[chAdesp] = stringArray[chAdesp+1];
-      stringArray[chAdesp+1] = aux;
-
-      let stDesplazada = stringArray.join("");
-      return stDesplazada;
-   }
    
+
    for (var i = 1; i <= long - 1; i++){
       workChar = palabra[i];
-      nBlqsEnI = Factorial(longHijo)/longHijo;
-
-      for (nBloq = 0; nBloq < nBlqsEnI; nBloq++) {
+      nBlqsEnI = Factorial(longHijo)/longHijo; //longHijo debe revalorizarse
+      
+      for (var nBloq = 0; nBloq < nBlqsEnI; nBloq++) {
          
-         if (i > 1) { //coge datos de snap y revaloriza (nos encontramos en S(3))
+         if (i > 1) { //(>=S3) coge datos de snap y calc CMCC
             //palHijo = snaps[] 
+            
+
+                        //S3 EN ADELANTE TO-DO//
+
+            // for (var j = 0; j <= longHijo; j++) {
                
+            //    palHijo = workChar + palHijo;
+            //    snapsBlqs[nBloq][0] = palHijo;
+            //    workChar = palHijo[0];
+            //    if(true){
+                  
+            //    }
+            //    else{ //movemos workingChar y creamos bloqueos
+            //       palHijo = desplazar(workChar,palHijo);
+            //       snapsBlqs[nBloq][j] = palHijo;
+            //    }
+            // }
          }
          else{ //APAÑO para S(2) para que no coja datos de snap pero lo "inicie"
-            let palHijo = palHijo + workChar;
-            snapsBlqs[0][0] = palHijo;
-            //let palHijo = workChar + palHijo;  ME DICE QUE YA HA SIDO DECLARADA!
-            snapsBlqs[0][1] = palHijo;
+
+            let palHijo = palHijo + workChar; //AB? longHijo=2?
+            var snapsBlqs = new Array(nBlqsEnI); //arrayI
+            
+            for (let nBlq = 0; nBlq < nBlqsEnI; nBlq++) {
+               snapsBlqs = new Array(longHijo); //arrayJ
+            }
+
+            snapsBlqs[0][0] = palHijo; //AB?
+            palHijo = workChar + palHijo; //ME DICE QUE YA HA SIDO DECLARADA?
+            snapsBlqs[0][1] = palHijo; //BA?
          }
 
-         for (var j = 0; j <= longHijo; j++) {
-            
-            palHijo = workChar + palHijo;
-            snapsBlqs[nBloq][0] = palHijo;
-            workChar = palHijo[0];
-            if(true){
-               
-            }
-            else{ //movemos workingChar y creamos bloqueos
-               palHijo = desplazar(workChar,palHijo);
-               snapsBlqs[nBloq][j] = palHijo;
-            }
-         }
       }
-         formatearAhorizontal(snapsBlqs,bloqueos,jCol);
-         //AQUI REVALORIZAR SNAPS una vez obtenidos CMCC
-            //por cada "Revalorizacion de snaps" los valores obtenidos
-            // por iterar la j , se trasladan(ordenadamente) a bloqueos donde j=0
+         snapsBlqs = formatearAhorizontal(snapsBlqs); //AQUI REVALORIZAR SNAPS una vez obtenidos CMCC
             //Nº bloqueos o snaps se establecen por Nº iteraciones de j.
    }
 
